344310afbc5a252cddc2fc884bc63c974020d564
kyourandesu <104125080+kyourandesu@users.noreply.github.com>
2023-09-27 19:38:23 +0000

    added portfolio pdf

--- react-app/src/components/Something.tsx
+++ react-app/src/components/Something.tsx
@@ -1,7 +1,15 @@
+// import ReactPDF
+// import ReactPDF from '@react-pdf/renderer'
+// ReactPDF.render(<MyDocument />, `public/portfolio internship.pdf`);
 function Something() {
     return (
         <>
-            <p id="02" className="something fw-bold mono display-6">Sorry, this website is still being developed. Come back soon!</p>
+            <div className="something">
+                <p id="02" className="fw-bold mono fs-5 d-flex justify-content-center p-5">Sorry, this website is still being developed but you can still view my portfolio pdf below. Come back soon!</p>
+                <iframe className="pdf  mx-auto" src="/public/portfolio internship.pdf">
+                </iframe>
+            </div>
+
         </>
     )
 }
